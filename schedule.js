75a90aca422b81e01a4387fef7ab1ca2e9911c3c
Itsadej Pongsapakpiboon <itsadake.p@ku.th>
2020-07-29 22:55:29 +0700

    4th assignment javascript

---
+++ schedule.js
@@ -0,0 +1,63 @@
+//Selecting Days
+function day1() {
+  var x = document.getElementById("daybtn1").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function day2() {
+  var x = document.getElementById("daybtn2").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function day3() {
+  var x = document.getElementById("daybtn3").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function day4() {
+  var x = document.getElementById("daybtn4").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function day5() {
+  var x = document.getElementById("daybtn5").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+
+//Selecting Courses
+function course1() {
+  var x = document.getElementById("coursebtn1").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course2() {
+  var x = document.getElementById("coursebtn2").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course3() {
+  var x = document.getElementById("coursebtn3").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course4() {
+  var x = document.getElementById("coursebtn4").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course5() {
+  var x = document.getElementById("coursebtn5").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course6() {
+  var x = document.getElementById("coursebtn6").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course7() {
+  var x = document.getElementById("coursebtn7").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course8() {
+  var x = document.getElementById("coursebtn8").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course9() {
+  var x = document.getElementById("coursebtn9").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
+function course10() {
+  var x = document.getElementById("coursebtn10").value;
+  document.getElementById("btnoutput").innerHTML = x;
+}
